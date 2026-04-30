4575e168d4810c77d613426cd91005f3c079ac0e
mak025 <mak025@edu.zealand.dk>
2026-04-30 12:45:58 +0200

    Force port 3000

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: "::",
+    port: 3000,
+    hmr: {
+      overlay: false,
+    },
+  },
   plugins: [vue()],
 })
+
+
